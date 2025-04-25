06f076598eb7230422bc2f588f17465fb05931a3
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-04-25 04:48:17 +0200

    completed the day 1 project

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,4 +1,4 @@
-username = input("What's your name?")
-length = len(username)
-print(length)
-
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in? \n")
+pet_name = input("What's the name of a pet?\n")
+print("Your band name could be " + city + " " + pet_name + ".")
