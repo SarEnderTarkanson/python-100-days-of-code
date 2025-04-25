48150daf6d548b013e28c363e9bc3e3c0cf5fb85
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-04-26 01:51:07 +0200

    Some exercises on types, len function and type conversion

--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,5 @@
-len(12345)
+name_of_the_user = input("Enter your name\n")
+user_name_length = len(name_of_the_user)
+user_name_str = str(user_name_length)
+
+print("Number of letters in your name: " + user_name_str)
