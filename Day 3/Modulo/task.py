bd75682d887081a0f6894df06880b15c5340444e
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-04-30 03:21:44 +0200

    modulo operator exercise

---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,6 @@
+number = int(input("Type a number please:\n"))
+
+if number % 2 == 0:
+    print("The number you inserted is an even number")
+else:
+    print("The number you inserted is an odd number")
