9d30ffc391c0adc4325728f36b8115bebb2cb8af
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-05-03 19:45:02 +0200

    Pizza Order Practice

--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -3,3 +3,24 @@ size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
+final_price = 0
+
+if size == "S":
+    final_price += 15
+elif size == "M":
+    final_price += 20
+elif size == "L":
+    final_price += 25
+else:
+    print("You typed a wrong input")
+
+if pepperoni == "Y":
+    if size == "S":
+        final_price += 2
+    else:
+        final_price += 3
+
+if extra_cheese == "Y":
+    final_price += 1
+
+print(f"Your final bill is: ${final_price}")
